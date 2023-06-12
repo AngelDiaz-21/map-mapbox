2351e6339fcdfb305c0c6169ae1d6140ee01ad2f
Angel <angelSistemas21@gmail.com>
2023-06-12 13:08:07 -0600

    change: name of constants

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('URL', '');
+    define('HOST', '127.0.0.1:3308');
+    define('DB', 'sig_mapa');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('CHARSET', 'utf8mb4');
+?>
