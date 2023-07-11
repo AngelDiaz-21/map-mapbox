d7b14efb1ba62eff4e28735d91ababee8b102338
Angel <angelSistemas21@gmail.com>
2023-07-11 16:14:59 -0600

    refactor: HTML structure

--- views/templates/contenido-imagenes.php
+++ views/templates/contenido-imagenes.php
@@ -1,150 +1,84 @@
-<div class="contenedor contenedor-grid">
+<div class="position-relative m-auto w-100">
     <div class="grid" id="grid">
-        <div class="categorias">
-            <button class="btn-regresar"><i class="fas fa-arrow-alt-circle-left"></i>Regresar</button>
-            <h3 class="subtitulos">Hospitales</h3>
+        <div class="hospitales">
+            <button class="btn-regresar text-white border-0 rounded-5">
+                <i class="fas fa-arrow-alt-circle-left"></i>Regresar
+            </button>
+            <h3 class="d-block text-center">Hospitales</h3>
             <!-- CREAMOS NUESTRA PROPIA CATEGORIA -->
-            <a href="https://hospitaldrdonatogalarcon.com/" data-categoria="hospital-donato" target="_blank">Hospital General Dr. Donato G. Alarcón<i class="fas fa-angle-right"></i></a>
-            <a href="https://www.facebook.com/HOSPITALGENERALACAPULCO" data-categoria="hospital-general" target="_blank">Hospital General Acapulco<i class="fas fa-angle-right"></i></a>
-            <a href="http://www.imss.gob.mx/" data-categoria="hospital-regional" target="_blank">Hospital General Regional Vicente Guerrero<i class="fas fa-angle-right"></i></a>
-            <a href="https://www.facebook.com/hgaensenanza/" data-categoria="hospital-issste" target="_blank">Hospital General ISSSTE<i class="fas fa-angle-right"></i></a>
-            <a href="#" data-categoria="hospital-militar" >Hospital Militar Regional<i class="fas fa-angle-right"></i></a>
-            <a href="#" data-categoria="hospital-naval" >Hospital Naval Acapulco<i class="fas fa-angle-right"></i></a>
+            <a href="https://hospitaldrdonatogalarcon.com/" data-hospital="hospital-donato" target="_blank">Hospital General Dr. Donato G. Alarcón<i class="fas fa-angle-right"></i></a>
+            <a href="https://www.facebook.com/HOSPITALGENERALACAPULCO" data-hospital="hospital-general" target="_blank">Hospital General Acapulco<i class="fas fa-angle-right"></i></a>
+            <a href="http://www.imss.gob.mx/" data-hospital="hospital-regional" target="_blank">Hospital General Regional Vicente Guerrero<i class="fas fa-angle-right"></i></a>
+            <a href="https://www.facebook.com/hgaensenanza/" data-hospital="hospital-issste" target="_blank">Hospital General ISSSTE<i class="fas fa-angle-right"></i></a>
+            <a href="#" data-hospital="hospital-militar">Hospital Militar Regional<i class="fas fa-angle-right"></i></a>
+            <a href="#" data-hospital="hospital-naval">Hospital Naval Acapulco<i class="fas fa-angle-right"></i></a>
         </div>
-
-        <div class="contenedor-subcategorias">
-            <div class="subcategoria" data-categoria="hospital-donato">
-
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Alarcon/Hospital-Donato-2.jpg" alt="">
-                    </a>
+        <div class="contenedor-imagenes" id="contenedor-imagenes">
+            <img class="message" alt="ilustración del icono 3d del edificio de la clínica del hospital png" fetchpriority="high" title="ilustración del icono 3d del edificio de la clínica del hospital" data-zoom-src="https://static.vecteezy.com/system/resources/previews/011/098/092/large_2x/hospital-clinic-building-3d-icon-illustration-png.png" data-action="click->resource-show-preview-zoom#trackZoomIn" data-resource-show-preview-target="previewImage" src="https://static.vecteezy.com/system/resources/previews/011/098/092/large_2x/hospital-clinic-building-3d-icon-illustration-png.png">
+            <div class="imagenes" data-hospital="hospital-donato">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Alarcon/Hospital-Donato-2.jpg" alt="Banner Hospital General Dr. Donato G. Alarcón">
                 </div>
-
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Alarcon/Hospital-Donato-1.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Alarcon/Hospital-Donato-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Alarcon/Hospital-Donato-3.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Alarcon/Hospital-Donato-4.jpg" alt="">
-                    </a>
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Alarcon/Hospital-Donato-1.jpg" alt="Hospital General Dr. Donato G. Alarcón">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Alarcon/Hospital-Donato-2.jpg" alt="Hospital General Dr. Donato G. Alarcón">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Alarcon/Hospital-Donato-3.jpg" alt="Hospital General Dr. Donato G. Alarcón">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Alarcon/Hospital-Donato-4.jpg" alt="Hospital General Dr. Donato G. Alarcón">
                 </div>
             </div>
-
-            <div class="subcategoria " data-categoria="hospital-general">
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-acapulco/hospital-general-banner.jpg" alt="">
-                    </a>
+            <div class="imagenes " data-hospital="hospital-general">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-acapulco/hospital-general-banner.jpg" alt="Banner Hospital General Acapulco">
                 </div>
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-acapulco/hospital-general-4.jpeg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-acapulco/hospital-general-2.jpeg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-acapulco/hospital-general-3.jpeg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-acapulco/hospital-general-1.jpg" alt="">
-                    </a>
-
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-acapulco/hospital-general-4.jpeg" alt="Hospital General Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-acapulco/hospital-general-2.jpeg" alt="Hospital General Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-acapulco/hospital-general-3.jpeg" alt="Hospital General Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-acapulco/hospital-general-1.jpg" alt="Hospital General Acapulco">
                 </div>
             </div>
-
-            <div class="subcategoria " data-categoria="hospital-regional">
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-banner.jpg" alt="">
-                    </a>
+            <div class="imagenes " data-hospital="hospital-regional">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-banner.jpg" alt="Banner Hospital General Regional Vicente Guerrero">
                 </div>
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-1.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-3.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-4.jpg" alt="">
-                    </a>
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-1.jpg" alt="Hospital General Regional Vicente Guerrero">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-2.jpg" alt="Hospital General Regional Vicente Guerrero">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-3.jpg" alt="Hospital General Regional Vicente Guerrero">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-regional-Vguerrero/hospital-vguerrero-4.jpg" alt="Hospital General Regional Vicente Guerrero">
                 </div>
             </div>
-
-            <div class="subcategoria " data-categoria="hospital-issste">
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-issste/hospital-general-issste-banner.jpg" alt="">
-                    </a>
+            <div class="imagenes " data-hospital="hospital-issste">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-issste/hospital-general-issste-4.jpg" alt="Banner Hospital General ISSSTE">                        
                 </div>
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-issste/hospital-general-issste-3.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-issste/hospital-general-issste-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-issste/hospital-general-issste-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-general-issste/hospital-general-issste-4.jpg" alt="">
-                    </a>
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-issste/hospital-general-issste-3.jpg" alt="Hospital General ISSSTE">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-issste/hospital-general-issste-2.jpg" alt="Hospital General ISSSTE">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-issste/hospital-general-issste-2.jpg" alt="Hospital General ISSSTE">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-general-issste/hospital-general-issste-banner.jpg" alt="Hospital General ISSSTE">
                 </div>
             </div>
-
-            <div class="subcategoria " data-categoria="hospital-militar">
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-militar/hospital-militar-banner.png" alt="">
-                    </a>
+            <div class="imagenes " data-hospital="hospital-militar">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-militar/hospital-militar-banner.png" alt="Banner Hospital Militar Regional">
                 </div>
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-militar/hospital-militar-1.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-militar/hospital-militar-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-militar/hospital-militar-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-militar/hospital-militar-4.jpg" alt="">
-                    </a>
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-militar/hospital-militar-1.jpg" alt="Hospital Militar Regional">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-militar/hospital-militar-2.jpg" alt="Hospital Militar Regional">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-militar/hospital-militar-2.jpg" alt="Hospital Militar Regional">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-militar/hospital-militar-4.jpg" alt="Hospital Militar Regional">
                 </div>
             </div>
-
-            <div class="subcategoria " data-categoria="hospital-naval">
-                <div class="banner-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-naval/hospital-naval-banner.jpg" alt="">
-                    </a>
+            <div class="imagenes " data-hospital="hospital-naval">
+                <div class="banner">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-naval/hospital-naval-banner.jpg" alt="Banner Hospital Naval Acapulco">
                 </div>
-                <div class="galeria-subcategoria">
-                    <a href="#">
-                        <img src="assets/img/Hospital-naval/hospital-naval-1.jpeg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-naval/hospital-naval-2.jpg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-naval/hospital-naval-3.jpeg" alt="">
-                    </a>
-                    <a href="#">
-                        <img src="assets/img/Hospital-naval/hospital-naval-4.jpg" alt="">
-                    </a>
+                <div class="galeria">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-naval/hospital-naval-1.jpeg" alt="Hospital Naval Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-naval/hospital-naval-2.jpg" alt="Hospital Naval Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-naval/hospital-naval-3.jpeg" alt="Hospital Naval Acapulco">
+                    <img src="<?php echo constant('URL');?>/public/assets/img/Hospital-naval/hospital-naval-4.jpg" alt="Hospital Naval Acapulco">
                 </div>
             </div>
         </div>
