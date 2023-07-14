d5aa313fed2f80d1fb00bc27aa2114e2ba9e98d9
Angel <angelSistemas21@gmail.com>
2023-07-13 18:34:06 -0600

    feat: Menu toggle

--- views/templates/menu.php
+++ views/templates/menu.php
@@ -1,25 +1,35 @@
+<!-- <a name="volver-arriba"></a> -->
 <header>
-            <nav class="menu" id="menu">
-                <div class="contenedor contenedor-botones-menu">
-                    <button id="btn-menu-barras" class="btn-menu-barras"><i class="fas fa-bars"></i></button>
-                    <button id="btn-menu-cerrar" class="btn-menu-cerrar"><i class="fas fa-times"></i></button>
+    <nav class="navbar navbar-expand-lg navbar-light text-center fw-bold">
+        <div class="container-fluid">
+            <div class="text-black p-2 shadow-lg rounded btn-departamentos">
+                <div class="d-flex align-items-center" id="btn-departamentos">
+                    <p class="btn-departamentos__p--font">Sistema de información <span class="btn-departamentos__span--font d-block">COVID-19</span></p>
+                    <i class="fas fa-caret-down"></i>
                 </div>
-                <div class="contenedor contenedor-enlaces-nav">
-                    <div class="btn-departamentos" id="btn-departamentos">
-                        <p>Sistema de información <span>COVID-19</span></p>
-                        <i class="fas fa-caret-down"></i>
-                    </div>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="menu__lista navbar-nav mb-2 mb-lg-0 fs-6">
+                    <li class="nav-item menu-link">
+                        <a class="nav-link" href="index.php">Hospitales cercanos</a>
+                    </li>
+                    <li class="nav-item menu-link">
+                        <a class="nav-link" href="inicio.php">Conoce tu ruta</a>
+                    </li>
+                    <li class="nav-item menu-link">
+                        <a class="nav-link" href="https://coronavirus.gob.mx/">Más información del COVID-19</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
 
-                    <div class="enlaces">
-                        <a href="index.php">Hospitales cercanos</a>
-                        <a href="inicio.php">Conoce tu ruta</a>
-                        <a href="https://coronavirus.gob.mx/" target="_blank">Más información del COVID-19</a>
-                        <!-- <input id="btn-abrir-popup" type="submit" class="main-header__link" value="Iniciar sesion"> -->
-                        <a href="inicia-sesion.php">Iniciar sesión</a>
-                    </div>
-                </div>
-                <?php
-                    include('templates/contenido-imagenes.php');
-                ?>
-            </nav>
-        </header>
+<?php
+    include('views/templates/contenido-imagenes.php');
+?>
