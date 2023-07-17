3638b629bb9c809cf0e1234ae8e9917dff772400
Angel <angelSistemas21@gmail.com>
2023-07-17 16:43:43 -0600

    add: Config file of database as example

--- config/config-example.php
+++ config/config-example.php
@@ -1,8 +1,8 @@
 <?php
     define('URL', 'http://localhost/map-mapbox/');
     define('HOST', '127.0.0.1:3308');
-    define('DB', 'sig_mapa');
-    define('USER', 'root');
+    define('DB', '');
+    define('USER', '');
     define('PASSWORD', '');
     define('CHARSET', 'utf8mb4');
 ?>
